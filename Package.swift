c791b1509b9bd0dad90782e9794bb517333ffea4
junseok.lee <junseok.lee@toss.im>
2024-05-04 20:47:01 +0900

    Dynamic 으로 수정 및 github.com 인증 안하도록

--- Package.swift
+++ Package.swift
@@ -11,8 +11,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppsFlyerLib",
-            url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework/releases/download/6.14.3/AppsFlyerLib-Static-SPM.xcframework.zip",
-            checksum: "53c824272e8729967af6b22e013577eccbc6e412b7e2da2805ddf3c1ecf7cfc5"
+            path: "BinaryTargets/AppsFlyerLib.xcframework"
         )
     ]
 )
